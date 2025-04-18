9b58eae3250c2b13ac990757b9dfff7f45653e7c
Pierre Oblin <pierre.oblin@orange.com>
2025-04-18 20:41:33 +0200

    activate CfEnv @Bean

--- src/main/java/com/orange/innov/xaas/mongodb/DemoApplication.java
+++ src/main/java/com/orange/innov/xaas/mongodb/DemoApplication.java
@@ -2,10 +2,18 @@ package com.orange.innov.xaas.mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.pivotal.cfenv.core.CfEnv;
 
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+    
+    @Bean
+    public CfEnv cfEnv() {
+      return new CfEnv();
+    }    
 }
